ae708a7eb65cb61ef797c7f22a8a2101a47c4707
François Rosé <francois.rose@soundcloud.com>
2019-10-06 22:15:17 +0200

    Add more sub-sections to std::sort with comparison function.

--- src/algorithms/sorting/25.7.1.1.sort.cpp
+++ src/algorithms/sorting/25.7.1.1.sort.cpp
@@ -14,9 +14,22 @@ TEST_CASE("25.7.1.1 sort") {
     CHECK_THAT(collection, Equals(std::vector<int>{0, 1, 2, 3}));
   }
 
-  SECTION("sort sorts a collection in place using a Comparison function") {
+  SECTION("sort sorts a collection in place using a comparison function") {
     auto collection = std::vector<int>{1, 3, 0, 2};
-    auto compare = std::greater<int>();
+    auto compare = std::function<bool(int, int)>();
+
+    SECTION("using a standard library compare function object") {
+      compare = std::greater<int>();
+    }
+    SECTION("using a lambda") {
+      compare = [](int a, int b) { return a > b; };
+    }
+    SECTION("using a custom function object") {
+      struct CustomLess {
+        bool operator()(int a, int b) const { return a > b; }
+      };
+      compare = CustomLess{};
+    }
 
     std::sort(collection.begin(), collection.end(), compare);
 
