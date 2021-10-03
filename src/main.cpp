8b35ee66a1b5676af2f332bd610c5f9778971c27
François Rosé <francois.rose@soundcloud.com>
2021-10-03 12:20:38 +0200

    Switch to Catch2 devel branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-#define CATCH_CONFIG_MAIN // catch provides the main function
+#include <catch2/catch_all.hpp>
 
-#include <catch2/catch.hpp>
-
-TEST_CASE("Catch is setup correctly")
-{
-    CHECK(1 + 1 == 2);
-}
+TEST_CASE("Catch is setup correctly") { CHECK(1 + 1 == 2); }
